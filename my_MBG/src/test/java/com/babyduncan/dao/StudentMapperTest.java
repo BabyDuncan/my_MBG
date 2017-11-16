4136f14bc2633918b82ffa64a97d52140a593178
GuohaoZhao <guohaozhao116008@sohu-inc.com>
2017-11-16 17:57:56 +0800

    增加单元测试

--- my_MBG/src/test/java/com/babyduncan/dao/StudentMapperTest.java
+++ my_MBG/src/test/java/com/babyduncan/dao/StudentMapperTest.java
@@ -1,11 +1,23 @@
 package com.babyduncan.dao;
 
+import com.babyduncan.model.StudentCriteria;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
 public class StudentMapperTest {
+
+    @Autowired
+    private StudentMapper studentMapper;
+
     @Test
     public void selectByExample() throws Exception {
-
+        StudentCriteria studentCriteria = new StudentCriteria();
+        System.out.println(studentMapper.selectByExample(studentCriteria));
     }
 
 }
