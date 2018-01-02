a3fc4181973049ec2b1f8f1304c52a5747d86f75
guohaozhao <guohaozhao116008@sohu-inc.com>
2018-01-02 17:09:59 +0800

    defaultModelType=flat

--- my_MBG/src/test/java/com/babyduncan/dao/StudentMapperTest.java
+++ my_MBG/src/test/java/com/babyduncan/dao/StudentMapperTest.java
@@ -1,6 +1,5 @@
 package com.babyduncan.dao;
 
-import com.babyduncan.model.StudentCriteria;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
